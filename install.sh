3183c40f792dbe74c25c955fd7e2430897919055
ankush <ankushmenat@gmail.com>
2015-03-27 20:28:38 +0530

    Fixed C library installation issue

--- install.sh
+++ install.sh
@@ -20,15 +20,23 @@ echo "Starting installation..."
 echo "This might take from 5-10 minute depending upon your internet speed."
 
 # updating packages
-apt-get update -qq && apt-get upgrade
+# apt-get update -qq && apt-get upgrade
 
 # installing some essentials and optional packages
-apt-get install build-essential clang make openjdk-7-jdk nodejs npm php5
+# apt-get install build-essential clang make openjdk-7-jdk nodejs npm php5
 
 # Installing cs50 library for C.
-wget -O cs50library.deb http://mirror.cs50.net/appliance50/2014/debs/dists/trusty/main/binary-i386/library50-c_6-0_i386.deb -qq
-dpkg -i cs50library.deb
-rm cs50library.deb
+cd library50-c-5
+gcc -c -ggdb -std=c99 cs50.c -o cs50.o
+ar rcs libcs50.a cs50.o
+chmod 0644 cs50.h libcs50.a
+mkdir -p /usr/local/include
+chmod 0755 /usr/local/include
+mv -f cs50.h /usr/local/include
+mkdir -p /usr/local/lib
+chmod 0755 /usr/local/lib
+mv -f libcs50.a /usr/local/lib
 
 echo "All done."
 echo "Now execute setup.sh as normal user."
+exit 0
