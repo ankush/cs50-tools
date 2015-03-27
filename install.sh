d1f30555350f39d6a00768dbd8517d02cf21f2a0
ankush <ankushmenat@gmail.com>
2015-03-27 20:41:26 +0530

    fixed minor bug in installer

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ ar rcs libcs50.a cs50.o
 chmod 0644 cs50.h libcs50.a
 mkdir -p /usr/local/include
 chmod 0755 /usr/local/include
-mv -f cs50.h /usr/local/include
+cp -f cs50.h /usr/local/include
 mkdir -p /usr/local/lib
 chmod 0755 /usr/local/lib
 mv -f libcs50.a /usr/local/lib
