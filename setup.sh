a7da12f91daa27ac1e0757ef3801848347bb91da
Ankush Menat <ankushmenat@gmail.com>
2015-04-05 20:55:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,8 +21,8 @@ cp style50.jar ~/cs50-tools
 
 echo "setting up aliases"
 echo 'alias make50='\''make CC=clang CFLAGS="-ggdb3 -O0 -std=c99 -Wall -Werror" LDLIBS="-lcs50 -lm -lcs"'\''' >> ~/.bashrc
-echo 'alias style50="/home/ankush/cs50-tools/style50.jar"' >> ~/.bashrc
-echo 'alias check50="/home/ankush/cs50-tools/check50/bin/check50"' >> ~/.bashrc
+echo 'alias style50="~/cs50-tools/style50.jar"' >> ~/.bashrc
+echo 'alias check50="~/cs50-tools/check50/bin/check50"' >> ~/.bashrc
 
 echo "Setting up SPL library"
 cd ~/cs50-tools/spl/
